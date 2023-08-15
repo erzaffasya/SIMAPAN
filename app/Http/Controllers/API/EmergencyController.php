d05a0025cbd631ad5a573538fed8eae0e4240103
Erza Fahmi Fasya <58721335+erzaffasya@users.noreply.github.com>
2023-08-16 01:03:00 +0800

    Update EmergencyController.php

--- app/Http/Controllers/API/EmergencyController.php
+++ app/Http/Controllers/API/EmergencyController.php
@@ -16,23 +16,24 @@ class EmergencyController extends Controller
         //     'gmaps' => 'nullable|string',
         //     'photo' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // Adjust the validation rules as needed
         // ]);
-
+        // return $request->file('photo');
         $filePath = null;
-        if ($request->file('file')->isValid()) {
-            $file = $request->file('file');
+        if ($request->file('photo') != null) {
+            // dd('asdas');
+            $file = $request->file('photo');
             $fileName = time() . '_' . $file->getClientOriginalName();
             $filePath = $file->storeAs('emergency', $fileName, 'public'); // Save the file to the storage/public/uploads directory
-        }    
+        }
 
 
         $task = Emergency::create([
             'judul' => $request->input('judul'),
-            'gmaps' => "https://www.google.com/maps?q=". $request->input('latitude'). ',' . $request->input('longitude'),
+            'gmaps' => "https://www.google.com/maps?q=" . $request->input('latitude') . ',' . $request->input('longitude'),
             'latitude' => $request->input('latitude'),
             'longitude' => $request->input('longitude'),
             'catatan' => $request->input('catatan'),
             'users_id' => Auth::user()->id,
-            'photo' => $filePath,
+            'photo' => "storage/". $filePath,
         ]);
 
         return response()->json($task, 201);
