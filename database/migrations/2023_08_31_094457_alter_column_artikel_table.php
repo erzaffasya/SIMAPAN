1b5dd97d5c899500d41a763533e1e3f682b03025
Erza Fahmi Fasya <58721335+erzaffasya@users.noreply.github.com>
2023-09-05 11:38:10 +0800

    Update 2023_08_31_094457_alter_column_artikel_table.php

--- database/migrations/2023_08_31_094457_alter_column_artikel_table.php
+++ database/migrations/2023_08_31_094457_alter_column_artikel_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnRoleUserTable extends Migration
+class AlterColumnArtikelTable extends Migration
 {
     /**
      * Run the migrations.
